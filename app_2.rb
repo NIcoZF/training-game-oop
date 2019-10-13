fc446c6f9c830ec3182345462957d5373dec07f6
NIcoZF <nico-7@hotmail.fr>
2019-10-13 19:35:18 +0200

    Human player class functional

---
+++ app_2.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts '-------------------------------------------------'
+puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "|Le but du jeu est d'être le dernier survivant !|"
+puts '-------------------------------------------------'
+
+# intialisation du jouer
+
+puts 'Quel est ton nom senseï'
+print ' > '
+player_1 = gets.chomp.to_s
+player_1 = HumanPlayer.new(player_1)
+
+# Créer un array d'ennemis
+enemies = []
+enemy_1 = Player.new('Josiane')
+enemy_2 = Player.new('José')
+enemies << enemy_1 << enemy_2
+
+while player_1.life_points > 0 && (enemy_1.life_points > 0 || enemy_2.life_points > 0)
+  puts player_1.show_state.to_s
+
+  puts "Quelle action veux-tu effectuer ?
+
+  a - chercher une meilleure arme
+  s - chercher à se soigner
+
+  attaquer un joueur en vue :
+  0 - Josiane a #{enemy_1.life_points} points de vie
+  1 - José a #{enemy_2.life_points} points de vie"
+
+  type = gets.chomp.to_s
+
+  case type
+  when "a"
+    player_1.search_weapon
+  when "s"
+    player_1.search_healt_pack
+  when 0
+    player_1.attacks(enemy_1)
+  when 1
+    player_1.attacks(enemy_2)
+    end
+  puts '                            '  
+  puts 'Les autres joueurs attaquent'
+  puts '                            '  
+
+  enemies.each do |enemy|
+    next if enemy.life_points <= 0
+    enemy.attacks(player_1)
+  end
+
+  puts 'La partie est finie'
+  if player_1.life_points > 0
+    puts 'BRAVO ! TU AS GAGNE !'
+  else
+    puts 'Loser ! Tu as perdu !'
+  end
+end
--- lib/player.rb
+++ lib/player.rb
@@ -9,7 +9,7 @@ class Player
   end
 
   def show_state
-    puts "Il reste #{life_points} points de vie à l'ami #{name}"
+    puts "Il reste #{@life_points} points de vie à l'ami #{@name}"
   end
 
   def gets_damage(strike)
@@ -31,5 +31,51 @@ class Player
 
   def compute_damage
     rand(1..6)
+  end
+end
+
+##############################################################################
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name)
+    @weapon_level = 1
+    @life_points = 100
+    super(name)
+  end
+
+  def show_state
+    puts "Le joueur #{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+    dice = rand(1..6)
+    weapon_found = dice
+    puts "Tu as trouvé une arme de niveau #{weapon_found}"
+    if weapon_found > @weapon_level
+      @weapon_level = weapon_found
+      puts ' Youhou tu viens de trouver une meilleure arme'
+    else
+      puts "Mauvaise pioche elle n'est pas mieux que ton arme actuelle"
+    end
+  end
+
+  def search_healt_pack
+    dice = rand(1..6)
+    case dice
+    when 1
+      puts "Tu n'as rien trouvé"
+    when 2..5
+      @life_points <= 50 ? @life_points += 50 : @life_points = 100
+      puts 'Bravo du as trouvé un pack de +50pts de vie'
+    when 6
+      @life_points <= 20 ? @life_points += 80 : @life_points = 100
+      puts 'Waow, tu as trouvé un pack de +80 points de vie !'
+    end
   end
 end
